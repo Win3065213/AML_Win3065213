ea9b501c5f94b17add5df76252473a6b909ff285
Win3065213 <AungNaing.N.Win@student.shu.ac.uk>
2025-01-06 15:43:13 +0000

    aria-label added

--- frontend/src/app/pages/discover/page.js
+++ frontend/src/app/pages/discover/page.js
@@ -84,7 +84,7 @@ export default function Media() {
               <option value="publisher">Publisher</option>
               <option value="year">Year</option>
             </select>
-            <button onClick={() => setSortAsc(!sortAsc)}>
+            <button aria-label='sorting order' onClick={() => setSortAsc(!sortAsc)}>
               {sortAsc ? 
                 <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-sort-ascending"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 6l7 0" /><path d="M4 12l7 0" /><path d="M4 18l9 0" /><path d="M15 9l3 -3l3 3" /><path d="M18 6l0 12" /></svg>
                 :<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-sort-descending"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 6l9 0" /><path d="M4 12l7 0" /><path d="M4 18l7 0" /><path d="M15 15l3 3l3 -3" /><path d="M18 6l0 12" /></svg>}
